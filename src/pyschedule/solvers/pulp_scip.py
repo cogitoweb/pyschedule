08652a33f82956daa0676fad867259809729306f
2zx <2zx@users.noreply.github.com>
2020-03-02 12:18:01 +0100

    typo

--- src/pyschedule/solvers/pulp_scip.py
+++ src/pyschedule/solvers/pulp_scip.py
@@ -7,7 +7,7 @@ import pulp.solvers
 
 class SCIP_CMD(pulp.solvers.LpSolver_CMD):
     def __init__(self, path = None, keepFiles = 0, mip = 1,
-            msg = 0, options = [], time_limit = None, ratio_gap = None
+            msg = 0, options = [], time_limit = None, ratio_gap = None,
             parallel = 0):
         pulp.solvers.LpSolver_CMD.__init__(self, path, keepFiles, mip, msg, options)
         self.time_limit = time_limit
